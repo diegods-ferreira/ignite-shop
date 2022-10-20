e80555f91ccd6ab67a6f8be3c1d0158e13b3d9d1
Diego Ferreira <diegosf1705@gmail.com>
2022-10-20 19:01:17 -0300

    Estrutura visual - Cabeçalho da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,20 @@ import { AppProps } from 'next/app';
 
 import { globalStyles } from '../styles/global';
 
+import { Container, Header } from '../styles/pages/app';
+
+import logoImg from '../assets/logo.svg';
+
 globalStyles();
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="Ignite Shop" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  );
 }
